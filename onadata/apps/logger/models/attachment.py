4179b2544fe449a93396b42a9ac77418efdb29ac
geomapdev <dreinhart@swca.com>
2017-03-08 21:05:33 +0000

    modified attachment folder structure

--- onadata/apps/logger/models/attachment.py
+++ onadata/apps/logger/models/attachment.py
@@ -6,15 +6,36 @@ from django.db import models
 
 from instance import Instance
 
+def get_first_value(d,value,default):
+    if value in d:
+        return d[value]
+    for k in d:
+        if isinstance(d[k], list):
+            for i in d[k]:
+                return get_first_value(i,value)
+    return default
 
 def upload_to(attachment, filename):
     instance = attachment.instance
     xform = instance.xform
+    instance_date = get_first_value(instance.json, 'today', None)
+    if not instance_date:
+        instance_time = get_first_value(instance.json, 'submission_time', None)
+        instance_date = instance_time[:10] if (instance_time and len(instance_time)>10) else None
+    instance_month = instance_date[:7] if len(instance_date)==10 else None
+    #if instance_date and instance_month:
+    #    final_date = os.path.join(instance_month,instance_date)
+    #else:
+    #    final_date = 'no_date'
     return os.path.join(
+	get_first_value(instance.json,'swca_office','no_office'),
+	get_first_value(instance.json,'project_number','no_project'),
+        instance_month,
+        instance_date,
         xform.user.username,
-        'attachments',
-        xform.uuid or 'form',
-        instance.uuid or 'instance',
+        #'attachments',
+        # xform.uuid or 'form',
+        #instance.uuid or 'instance',
         os.path.split(filename)[1])
 
 
